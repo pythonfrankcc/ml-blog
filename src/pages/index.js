8d55fcccf42a8c6ab1bad8bd4afebcdce38701f2
kibuikaCodes <kibuikasteve@gmail.com>
2020-02-08 14:10:53 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+// import SEO from "../components/seo"
+// import Button from "../components/button"
+
+class IndexPage extends React.Component {
+  render() {
+
+    // change the title here
+    // const siteTitle = "CC Blog"
+
+    return (
+      <Layout location={this.props.location} >
+        {/* <SEO
+          title="Home"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        /> */}
+        {/* change the image here */}
+        <img style={{ margin: 0 }} src="./mara.jpg" alt="The mara" />
+        <h1>
+          Hey people{" "}
+          <span role="img" aria-label="wave emoji">
+            👋
+          </span>
+        </h1>
+        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>
+          This starter comes out of the box with styled components and Gatsby's
+          default starter blog running on Netlify CMS.
+        </p>
+        <p>Now go build something great!</p>
+         {/* href to the blog  */}
+        <a href='https://www.youtube.com' target="_blank">
+          Go
+        </a>
+      </Layout>
+    )
+  }
+}
+
+export default IndexPage
